815ec7fbd65e8f9188e7da0b9cc4fa2a770bdee3
moemnew1 <moemenw1@gmail.com>
2024-07-17 06:07:22 +0300

    install jetstream and add its needs

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->integer('role')->comment('0 user - 1 secertary - 2 doctor - 3 owner')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
 
