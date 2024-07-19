e67877178c905514d46a3c01a80ef7856a1007b6
mohamedahmed222004 <moeid22ii@gmail.com>
2024-07-19 18:31:33 +0300

    finsh booking model and migration

--- app/Models/booking.php
+++ app/Models/booking.php
@@ -5,7 +5,53 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class booking extends Model
+class Booking extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'booking_type_id',
+        'user_id',
+        'branch_id',
+        'doctor_id',
+        'status',
+    ];
+
+    /**
+     * Get the booking type that the booking belongs to.
+     */
+    public function bookingType()
+    {
+        return $this->belongsTo(BookingType::class);
+    }
+
+    /**
+     * Get the user that made the booking.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the branch where the booking is made.
+     */
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    /**
+     * Get the doctor for the booking.
+     */
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
--- database/migrations/2024_07_19_004906_create_bookings_table.php
+++ database/migrations/2024_07_19_004906_create_bookings_table.php
@@ -4,24 +4,39 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBookingsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->unsignedBigInteger('booking_type_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('branch_id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->boolean('status')->comment('not paid 0, paid 1');
             $table->timestamps();
+
+            $table->foreign('booking_type_id')->references('id')->on('booking_types')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('branch_id')->references('id')->on('branches')->onDelete('cascade');
+            $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('bookings');
     }
-};
+}
