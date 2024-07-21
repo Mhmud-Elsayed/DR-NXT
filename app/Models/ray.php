3230ae00bb9967e1247a08ff423d2d448836ef86
moemnew1 <moemenw1@gmail.com>
2024-07-22 02:30:37 +0300

    new names for models

--- app/Models/ray.php
+++ app/Models/ray.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ray extends Model
+class Ray extends Model
 {
     use HasFactory;
 
